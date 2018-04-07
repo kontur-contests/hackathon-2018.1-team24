c4988e8345b36def2d01935617018510c961d18b
ivanov.av <ivanov.av@skbkontur.ru>
2018-04-07 20:06:38 +0500

    Бизнес логика

--- ServerBackend/GameCoreLibrary/Player.cs
+++ ServerBackend/GameCoreLibrary/Player.cs
@@ -4,15 +4,12 @@ namespace GameCoreLibrary
 {
     public class Player : BaseGameObject
     {
-        public const int HitRange = 100;
-
         public string PlayerName { get; set; }
         public int Score { get; set; }
 
         public List<Modificator> Modificators { get; set; }
         public int BaseHitPoints { get; set; }
-        public float Speed { get; set; }
-
+        
         public List<Skill> Skills { get; set; }
 
         public override ObjectType ObjectType
