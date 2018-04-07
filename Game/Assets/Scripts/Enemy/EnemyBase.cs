5ee44eebf30991a3213a773c530280ce017c2099
ivanov.av <ivanov.av@skbkontur.ru>
2018-04-07 17:05:11 +0500

    Enemy model

---
+++ Game/Assets/Scripts/Enemy/EnemyBase.cs
@@ -0,0 +1,26 @@
+﻿namespace Assets.Scripts.Enemy
+{
+    public abstract class EnemyBase
+    {
+        protected EnemyBase(double speed, double hit, double hp)
+        {
+            Speed = speed;
+            Hit = hit;
+            Hp = hp;
+        }
+
+        public double Speed { get; private set; }
+        public double Hit { get; private set; }
+        public double Hp { get; private set; }
+
+        public bool IsAlive
+        {
+            get { return Hp > 0; }
+        }
+
+        public void ApplyHit(double hit)
+        {
+            Hp -= hit;
+        }
+    }
+}
