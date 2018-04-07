524a5480ecbbc088304ca00892cf537e38d698f9
berezniker <berezniker@skbkontur.ru>
2018-04-07 14:18:42 +0500

    initial commit for server backend

---
+++ ServerBackend/MultiplayerSynchronizer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Fleck;
+
+namespace MultiplayerSynchronizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new WebSocketServer("ws://0.0.0.0:8181");
+            var allSockets = new List<IWebSocketConnection>();
+            server.Start(socket =>
+            {
+                socket.OnOpen = () =>
+                {
+                    Console.WriteLine("Open!");
+                    allSockets.Add(socket);
+                };
+                socket.OnClose = () =>
+                {
+                    Console.WriteLine("Close!");
+                    allSockets.Remove(socket);
+                };
+                socket.OnMessage = message =>
+                {
+                    Console.WriteLine(message);
+                    allSockets.ToList().ForEach(s => s.Send("Echo: " + message));
+                };
+            });
+            var input = Console.ReadLine();
+        }
+    }
+}
