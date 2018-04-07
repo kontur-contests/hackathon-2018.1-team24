e97494dfa05e4f8d7b1e7f785b20160b72986a1e
ARTEMPC\kilos <kilosoft@mail.ru>
2018-04-07 13:46:10 +0300

    Movement System Part 1

---
+++ Game/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float maxSpeed = 5f;
+    public float jumpSpeed = 30f;
+
+    private bool jump;
+
+    private Transform groundCheck;
+    private bool grounded = false;
+
+    public PlayerPhysics playerPhysics;
+
+    private void Start()
+    {
+        playerPhysics = GetComponent<PlayerPhysics>();
+        groundCheck = transform.Find("groundCheck");
+    }
+
+    private void Update()
+    {
+        var move = new Vector2(Input.GetAxis("Horizontal") * Time.deltaTime * maxSpeed, playerPhysics.rig.velocity.y);
+
+        //даем силу движения игроку
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetMouseButton(0)) && grounded)
+        {
+            playerPhysics.rig.AddForce(Vector2.up * jumpSpeed);
+        }
+
+
+
+        playerPhysics.rig.velocity = move;
+    }
+
+}
---
+++ Game/Assets/Scripts/Player/PlayerPhysics.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPhysics : MonoBehaviour {
+
+    public Rigidbody2D rig;
+	void Awake ()
+    {
+        rig = GetComponent<Rigidbody2D>();
+	}
+
+
+    public void Move(Vector2 moveAmount, bool jump)
+    {
+        if (jump)
+        {
+            rig.AddForce(new Vector2(0, 50));
+        }
+        var pos = new Vector2(rig.position.x + moveAmount.x, rig.position.y + moveAmount.y + Physics2D.gravity.y * Time.deltaTime);
+        //rig.MovePosition(pos);
+    }
+
+	void Update () {
+		
+	}
+}
