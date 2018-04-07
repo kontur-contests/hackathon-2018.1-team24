0d991f7d1de0bf8c0f9936751e183bc7fd72bd59
ivanov.av <ivanov.av@skbkontur.ru>
2018-04-07 22:37:40 +0500

    Sync

--- ServerBackend/GameCoreLibrary/GameLevel.cs
+++ ServerBackend/GameCoreLibrary/GameLevel.cs
@@ -11,5 +11,25 @@ namespace GameCoreLibrary
         {
             GameObjects = level.GameObjects.Select(x => x.Clone()).ToList();
         }
+
+        public void DoSync(IList<IGameObject> gameObjects)
+        {
+            foreach (var gObject in gameObjects)
+            {
+                var existing = GameObjects.Find(x => x.Id == gObject.Id);
+                if(existing == null)
+                    GameObjects.Add(gObject);
+                else
+                {
+                    existing.Pos = gObject.Pos;
+                    existing.HealthPoints = gObject.HealthPoints;
+                }
+            }
+
+            foreach (var remove in GameObjects.Where(x=> x.ObjectType != ObjectType.Player && gameObjects.All(go => go.Id != x.Id)).ToArray())
+            {
+                GameObjects.Remove(remove);
+            }
+        }
     }
 }
